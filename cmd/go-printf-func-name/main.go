56692901402e69cf34657a08e5fbd6e7665e241f
Cooperjie <782190764@qq.com>
2023-09-14 16:36:21 +0800

    first commit test analysis

---
+++ cmd/go-printf-func-name/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main()  {
+	//singlechecker.Main(analyzer.Analyzer)
+}
---
+++ testdemo/astdemo/mylog.go
@@ -0,0 +1,9 @@
+//example.go
+package main
+
+import "log"
+
+func myLog(format string, args ...interface{}) {
+	const prefix = "[my] "
+	log.Printf(prefix + format, args...)
+}
