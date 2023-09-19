9112a81dc045a99eb41ff508469140bce38e4f7a
Cooperjie <782190764@qq.com>
2023-09-19 16:03:13 +0800

    Initial commit

--- cmd/go-printf-func-name/main.go
+++ cmd/go-printf-func-name/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "golang.org/x/tools/go/analysis/singlechecker"
+import "github.com/Cooperjie/SastGo"
+
 func main()  {
-	//singlechecker.Main(analyzer.Analyzer)
+	singlechecker.Main(analyzer.Analyzer)
 }
